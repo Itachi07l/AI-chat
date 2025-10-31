b6ce90330a94540494e11c7a0b90351b92a01dc9
Sanchit Tikhile <157561024+Itachi07l@users.noreply.github.com>
2025-10-31 18:45:23 +0530

    Update server.js

--- AI-chat/backend/server.js
+++ AI-chat/backend/server.js
@@ -3,10 +3,11 @@ const app=require('./src/app');
 const initSockets=require('./src/sockets/socket.server');
 const { createServer } = require("http");
 const httpServer = createServer(app); 
-   
+    
 initSockets(httpServer);
 httpServer.listen(3000, () =>{    
     console.log(`Server started on port 3000`) 
 });
+
 
 
